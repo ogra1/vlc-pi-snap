f0e5e623e2a1d39b4f94fd07a2a940d14a22ae3b
Oliver Grawert <ogra@ubuntu.com>
2020-12-21 23:15:24 +0100

    add first start checks and visual feedback for locale generation

---
+++ scripts/checks.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+[ -e "$SNAP_USER_DATA/.check-complete" ] && exec "$@"
+
+if ! id -Gn | grep -q video; then
+  yad --text "<b>Error, can not open video playback device \!</b>\n\nThe user $LOGNAME is not in the "video" group.\nPlease execute:\n\n    <b>sudo usermod -a -G video $LOGNAME</b>\n\nIn a Terminal, afterwards log out and back in again." \
+    --borders=5 --center --on-top --undecorated \
+    --skip-taskbar && exit 0
+fi
+
+if ! snapctl is-connected hardware-observe; then
+  yad --text "<b>Error, can not get status of video playback device \!</b>\n\nPlease connect the hardware-observe snap interface\nwith the command:\n\n    <b>sudo snap connect vlc-pi:hardware-observe</b>\n\nOr through the Permissions UI in the snap-store application" \
+    --borders=5 --center --on-top --undecorated \
+    --skip-taskbar && exit 0
+fi
+
+if [ ! -d "$SNAP_USER_COMMON/.local/lib/locale/$LANG" ]; then
+  yad --text "\nGenerating VLC translations for:\n\n\t$LANG" \
+    --borders=10 --no-buttons --center --on-top \
+    --undecorated --skip-taskbar --timeout=5 \
+    --timeout-indicator=bottom &
+fi
+
+touch $SNAP_USER_DATA/.check-complete
+exec "$@"
